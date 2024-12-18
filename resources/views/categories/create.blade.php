4cc3830efae70dea790092facfddf42175e19daf
ZayYarNaing <zayarnaing.pp@gmail.com>
2024-12-18 11:55:18 +0630

    Update Category UI

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -1,19 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Category</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
-    <div>
-        <h1>This is Category Create</h1>
-        <form action="{{ route('categories.store') }}" method="POST">
-            @csrf
-            <input type="text" placeholder="Enter Category Name" name="name"/>
-            <button type="submit">+Create</button>
-        </form>
+    <div class="container">
+        <div class="card mt-4">
+            <div class="card-header">
+                Create Category
+            </div>
+            <form action="{{ route('categories.store') }}" method="POST">
+                @csrf
+                <input type="text" placeholder="Enter Category Name" name="name" class="form-control card-body" />
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-primary">
+                        +Create
+                    </button>
+                    <a href="{{ route('categories.index') }}" class="btn btn-secondary">Back</a>
+                </div>
+            </form>
+        </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous">
+    </script>
+
 </body>
+
 </html>
